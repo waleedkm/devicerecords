435573c4bcc60d7024b29bceee5605debe43a23c
Waleed <waleedkm60@gmail.com>
2018-07-22 06:49:24 +0000

    Schema moduls got created Commmit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true }));
 mongoose.connect("mongodb://localhost:27017/pickerdevice", {useNewUrlParser:true});
 
 var Schema = mongoose.Schema
-//Shema definitions
+//Schema definitions
 
 var userSchema = new Schema({
     
